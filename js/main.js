802939c3efef08058aa584a094c3cd134b79cf58
Daniel Vargas García <161933152+DaniVG7@users.noreply.github.com>
2024-07-05 17:28:56 +0200

    ifsNotAnids

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 //lazy load
 const sections = document.querySelectorAll("[data-src]");
 for (const section of sections) {
-  const response = await fetch(section.dataset.src);
-  const mainContent = await response.text();
-  section.outerHTML = mainContent;
+  const RESPONSE = await fetch(section.dataset.src);
+  const MAIN_CONTENT = await RESPONSE.text();
+  section.outerHTML = MAIN_CONTENT;
 }
 
 
@@ -13,16 +13,16 @@ inputEmail.addEventListener("blur", function () {
   let emailPattern = /@gmail\.?/i;
   let emailWarning = document.getElementById("emailWarning");
 
-  if (email) {
-    if (emailPattern.test(email)) { // si se cumple el patern de la variable que creamos (es @gmail), mostramos esta advertencia
-      emailWarning.style.display = "block";
-      emailWarning.innerHTML = "The email domain cannot be gmail❗";
-    } else {
-      emailWarning.style.display = "none";
-    }
-  } else {
+  if (!email) {
     emailWarning.innerHTML = "Please enter a valid email❗"; // Mostramos este mensaje si el campo está vacío
-    emailWarning.style.display = "block"; 
+    emailWarning.style.display = "block";
+    return 0;
+  }
+  if (emailPattern.test(email)) { // si se cumple el patern de la variable que creamos (es @gmail), mostramos esta advertencia
+    emailWarning.style.display = "block";
+    emailWarning.innerHTML = "The email domain cannot be gmail❗";
+  } else {
+    emailWarning.style.display = "none";
   }
 });
 
@@ -31,16 +31,16 @@ inputPhone.addEventListener("blur", function () {
   let phone = document.getElementById("inputPhone").value;
   let phonePattern = /^[679]\d{8}(\s.*)?$/;
   let phoneWarning = document.getElementById("phoneWarning");
-  if(phone){                             //Si el usuario pone algo en el campo de telefono ejecutamos la siguiente validación...
-    if (!phonePattern.test(phone) && phone) {
-      phoneWarning.style.display = "block";
-      phoneWarning.innerHTML = "Phone number must start with 6, 7 or 9 and be 9 digits long. You can add the country extension after the phone number leaving an space between them❗";
-    } else {
-      phoneWarning.style.display = "none";
-    }
-  }else{
+  if (!phone) {                             //Si el usuario pone algo en el campo de telefono ejecutamos la siguiente validación...
     phoneWarning.innerHTML = "This field cannot be empty❗";
     phoneWarning.style.display = "block"; // Mostramos este mensaje si el campo está vacío
+    return 0;
+  }
+  if (!phonePattern.test(phone)) {
+    phoneWarning.style.display = "block";
+    phoneWarning.innerHTML = "Phone number must start with 6, 7 or 9 and be 9 digits long. You can add the country extension after the phone number leaving an space between them❗";
+  } else {
+    phoneWarning.style.display = "none";
   }
 });
 
